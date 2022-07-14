a9e9497ced72e098db1c913c0b1487b5a06a5d47
isuru10 <tmisuru@outlook.com>
2022-07-15 00:32:34 +0530

    Removed tvdb lookup from config

--- sort_after_done.sh
+++ sort_after_done.sh
@@ -1,1 +1,2 @@
-sudo nice perl /home/pi/sorttv/sorttv.pl >> sort_after_done.log
+nice perl /home/pi/sorttv/sorttv.pl > /home/pi/sorttv/sort_after_done.log
+/home/pi/remove_completed_torrents.sh > /home/pi/remove_completed_torrents.log
